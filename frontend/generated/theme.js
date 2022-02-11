ebbb3be996f9c22c65aa6862020fa1f2acbbc6f7
Loic Geffroy <loic.geffroy@masagroup.net>
2022-02-11 15:39:51 +0000

    add initial project files.

---
+++ frontend/generated/theme.js
@@ -0,0 +1,2 @@
+import {applyTheme as _applyTheme} from 'generated/theme-gitw.generated.js';
+export const applyTheme = _applyTheme;
---
+++ src/main/java/com/lgylgy/gitw/views/about/AboutView.java
@@ -0,0 +1,31 @@
+package com.lgylgy.gitw.views.about;
+
+import com.lgylgy.gitw.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("About")
+@Route(value = "about", layout = MainLayout.class)
+public class AboutView extends VerticalLayout {
+
+    public AboutView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
