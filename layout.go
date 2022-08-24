666f72e708aae0656f716521f3b0bf0681b31500
Loic Geffroy <loic.geffroy@masagroup.net>
2022-08-24 14:40:17 +0100

    improve error message.

--- layout.go
+++ layout.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jroimartin/gocui"
 	"github.com/lgylgy/gitw/gui"
 )
 
 type View interface {
 	Draw(g *gocui.Gui) error
+	GetName() string
 }
 
 type Layout struct {
@@ -28,7 +31,7 @@ func (l *Layout) Draw(g *gocui.Gui) error {
 	for _, view := range l.views {
 		err := view.Draw(g)
 		if err != nil {
-			return err
+			return fmt.Errorf("view %s: %v", view.GetName(), err)
 		}
 	}
 	return nil
